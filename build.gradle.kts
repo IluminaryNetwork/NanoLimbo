38e1f4f6721f5239c44e3a1e9160b56221be4e1b
Valentine <21033866+BoomEaro@users.noreply.github.com>
2025-11-29 15:36:29 +0200

    Migrate to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.9"
+    id("com.github.gmazzo.buildconfig") version "6.0.6"
+}
+
+group = "ua.nanit"
+version = "1.9.10"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("org.spongepowered:configurate-yaml:4.2.0")
+
+    implementation("io.netty:netty-handler:4.2.7.Final")
+    implementation("io.netty:netty-transport-native-epoll:4.2.7.Final:linux-x86_64")
+    implementation("io.netty:netty-transport-native-epoll:4.2.7.Final:linux-aarch_64")
+
+    implementation("net.kyori:adventure-nbt:4.25.0")
+    implementation("com.google.code.gson:gson:2.13.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+
+buildConfig {
+    className("BuildConfig")
+    packageName("ua.nanit.limbo")
+    buildConfigField("LIMBO_VERSION", provider { "${project.version}" })
+}
+
+tasks.shadowJar {
+    from("LICENSE")
+
+    archiveClassifier.set("")
+    archiveVersion.set("")
+
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to "ua.nanit.limbo.NanoLimbo"
+            )
+        )
+    }
+
+    minimize {
+        exclude(dependency("ch.qos.logback:logback-classic:.*:.*"))
+    }
+}
